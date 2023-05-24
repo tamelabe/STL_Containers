8db71897d4de91b3e9674de71504353d8ac41d3e
Kadyr Atakhan <kadyr.atakhanov@gmail.com>
2023-05-24 06:18:35 +0300

    resloved threads in review

--- src/s21_Vector.h
+++ src/s21_Vector.h
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#pragma once
 #ifndef CPP2_S21_CONTAINERS_S21_VECTOR_H_
 #define CPP2_S21_CONTAINERS_S21_VECTOR_H_
 
