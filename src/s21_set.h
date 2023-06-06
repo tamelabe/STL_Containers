6381e581f2b0514f2953b07834d1c9d7cb220fbd
darrpama <darrpama@student.21-school.ru>
2023-06-06 23:25:57 +0300

    init commit

--- src/s21_set.h
+++ src/s21_set.h
@@ -1,8 +1,11 @@
 #include "s21_tree.h"
 
 namespace s21 {
-    template<typename V, typename Compare = std::less<V>>
-    class set : public s21::BSTree<V, Compare> {
 
-    };
+template<typename V, typename Compare = std::less<V>>
+class set : public s21::BSTree<V, Compare> {
+
+};
+
+
 }
