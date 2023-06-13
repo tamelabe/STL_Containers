0ca909fb73221cf7f04c62e9cc071af561a0ec14
darrpama <darrpama@student.21-school.ru>
2023-06-13 23:09:53 +0300

    add implementation for two constructors

--- src/s21_set.h
+++ src/s21_set.h
@@ -1,40 +1,22 @@
-<<<<<<< HEAD
-/**
-Copyright 2023 darrpama@student.21-school.ru, myregree@student.21-school.ru
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-  http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-**/
-
-#include "s21_tree.h"
-=======
+#ifndef CPP2_S21_CONTAINERS_S21_SET_H_
+#define CPP2_S21_CONTAINERS_S21_SET_H_
 #include "helpers/BinaryTree.h"
->>>>>>> 4e19fc7a8e4cdb8bf0807f757cd0e2021d94dbae
 
 namespace s21 {
 
-template<typename Key>
+template<typename KT>
 class set {
-  using key_type = Key;
-  using value_type = Key;
-  using reference = value_type &;
+  using key_type        = KT;
+  using value_type      = KT;
+  using reference       = value_type &;
   using const_reference = const value_type &;
-  using iterator = SetIterator<key_type>;
-  using const_iterator = SetConstIterator<key_type>;
-  using size_type = size_t;
+  using iterator        = SetIterator<key_type>;
+  using const_iterator  = SetConstIterator<key_type>;
+  using size_type       = size_t;
 
 public:
   set();
-// | set(std::initializer_list<value_type> const &items);
+  set(std::initializer_list<value_type> const &items);
 // | set(const set &s);
 // | set(set &&s);
 // | ~set();
@@ -57,12 +39,23 @@ public:
   // bool contains(const Key& key);
 
 private:
-  s21::BSTree<Key> tree_;
-
+  s21::BinaryTree<key_type, value_type> tree_;
+  size_type size_;
 };
 
-template <typename Key>
-s21::set<Key>::set() {}
+template <typename KT>
+s21::set<KT>::set() : tree_(), size_(0) {}
 
+template <typename KT>
+s21::set<KT>::set(std::initializer_list<value_type> const &items) : tree_(), size_(0) {
+  for (auto item : items) {
+    tree_.insert(item, 0);
+    size_++;
+  }
 }
+
+}  // namespace s21
+
+
 // mintimir izvrachenec - on lubit VIM
+#endif  // CPP2_S21_CONTAINERS_S21_SET_H_
