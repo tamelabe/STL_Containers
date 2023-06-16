cca8099d0ba2d0eabf78f5771d5ddf12cf42fa8f
darrpama <darrpama@student.21-school.ru>
2023-06-16 22:17:05 +0300

    add move tests and comparisonSet()

--- src/tests/s21_set_test.cc
+++ src/tests/s21_set_test.cc
@@ -7,6 +7,12 @@
 #include <iostream>
 #include <vector>
 
+
+template<class T>
+bool comparisonSet(s21::set<T> &s21_set, s21::set<T> &s21_set2);
+template<class T>
+bool comparisonSet(s21::set<T> &s21_set, std::set<T> &std_set);
+
 // Constructors
 TEST(member_functions, base_constructor) {
   s21::set<int> set;
@@ -42,9 +48,33 @@ TEST(member_functions, copy_constructor) {
     4
   };
   s21::set<int> copySet(set);
-  ASSERT_TRUE(copySet.size() == set.size());
-  ASSERT_TRUE(copySet.begin() == set.size());
+  ASSERT_TRUE(comparisonSet(set, copySet));
+}
 
+TEST(member_functions, move_constructor) {
+  s21::set<int> set {
+    1,
+    2,
+    3,
+    4
+  };
+  s21::set<int> copySet(set);
+  s21::set<int> moveSet(std::move(set));
+  ASSERT_TRUE(!comparisonSet(set, moveSet));
+  ASSERT_TRUE(comparisonSet(moveSet, copySet));
+}
+
+TEST(member_functions, move_operator) {
+  s21::set<int> set {
+    1,
+    2,
+    3,
+    4
+  };
+  s21::set<int> copySet(set);
+  s21::set<int> moveSet = std::move(set);
+  ASSERT_TRUE(!comparisonSet(set, moveSet));
+  ASSERT_TRUE(comparisonSet(moveSet, copySet));
 }
 
 int main(int argc, char** argv) {
@@ -54,5 +84,28 @@ int main(int argc, char** argv) {
 
 template<class T>
 bool comparisonSet(s21::set<T> &s21_set, std::set<T> &stl_set) {
-  if (s21_set.size() != stl_set.size()) {}
+  if (s21_set.size() != stl_set.size() || 
+      s21_set.empty() != stl_set.empty()) {
+    return false;
+  }
+  auto it1 = s21_set.begin();
+  auto it2 = stl_set.begin();
+  for (size_t size = 0; size < s21_set.size(); ++size, ++it1, ++it2) {
+    if (*it1 != *it2) return false;
+  }
+  return true;
+}
+
+template<class T>
+bool comparisonSet(s21::set<T> &s21_set, s21::set<T> &s21_set2) {
+  if (s21_set.size() != s21_set2.size() || 
+      s21_set.empty() != s21_set2.empty()) {
+    return false;
+  }
+  auto it1 = s21_set.begin();
+  auto it2 = s21_set2.begin();
+  for (size_t size = 0; size < s21_set.size(); ++size, ++it1, ++it2) {
+    if (*it1 != *it2) return false;
+  }
+  return true;
 }
